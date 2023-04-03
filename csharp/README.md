30a280f3fce7c1321117ae8a3157a5bcba0e674e
nagyzekkyandras <nagyandris0718@gmail.com>
2023-04-03 11:40:56 +0200

    csharp init

---
+++ csharp/README.md
@@ -0,0 +1,10 @@
+# C# development in VSCode on MacOS
+
+Commands:
+```sh
+# create new console application in current folder
+dotnet new console
+
+# run the applicaiton
+dotnet run
+```
---
+++ csharp/training/modules/csharp-basic-formatting/Program.cs
@@ -0,0 +1,29 @@
+﻿// sor törés
+Console.WriteLine("Hello\nWorld!");
+// tabulálás
+Console.WriteLine("Hello\tWorld!");
+// escape karakterek
+Console.WriteLine("Hello \"World\"!");
+Console.WriteLine("c:\\source\\repos");
+// formatting
+// example 1
+Console.WriteLine("Generating invoices for customer \"Contoso Corp\"\' ...\n");
+Console.WriteLine("Invoice: 1021\t\tComplete!");
+Console.WriteLine("Invoice: 1022\t\tComplete!");
+Console.WriteLine("\nOutput Directory:\t");
+Console.WriteLine(@"    c:\source\repos    
+        (this is where your code goes)");
+Console.WriteLine(@"c:\invoices");
+
+// using variables
+string firstName = "Bob";
+string message = "Hello " + firstName;
+Console.WriteLine(message);
+
+string message2 = $"Hello {firstName}!";
+Console.WriteLine(message2);
+
+int version = 11;
+string updateText = "Update to Windows";
+string message3 = $"{updateText} {version}";
+Console.WriteLine(message3);
